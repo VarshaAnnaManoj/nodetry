cb5ca2cea2e8f1e42ad243c24a1a79f51f9e097e
Manoj Sebastian <varinfoserv@gmail.com>
2026-05-31 17:03:16 -0400

    test

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Backend  NowRunning");
+  res.send("Backend Now Running");
 });
 
 app.get("/api/message", (req, res) => {
@@ -15,11 +15,13 @@ app.get("/api/message", (req, res) => {
     message: "API is Working"
   });
 });
+
 app.get("/api/states", async (req, res) => {
-
   const users = await prisma.states.findMany();
-
   res.json(users);
-
 });
-export default app;
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
