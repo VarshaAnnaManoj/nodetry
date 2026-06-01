0e81d275b67e1dd4386e10e9ee9f41f88e302baa
Manoj Sebastian <varinfoserv@gmail.com>
2026-06-01 02:39:37 -0400

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { useEffect, useState } from "react";
+import api from "./services/api";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://backend:5000',
-        changeOrigin: true
-      }
-    }
-  }
-})
+function App() {
+
+  const [states, setStates] = useState([]);
+
+  useEffect(() => {
+
+    api.get("/states")
+      .then((response) => {
+
+        
+        console.log("TYPE:", typeof response.data);
+        console.log("DATA:", response.data);
+        console.log("IS ARRAY:", Array.isArray(response.data));
+
+        if (Array.isArray(response.data)) {
+          setStates(response.data);
+        }
+
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }, []);
+
+  return (
+    <div>
+
+      <h1>States</h1>
+
+      {states.map((state) => (
+        <div key={state.id}>
+          {state.name}
+        </div>
+      ))}
+
+    </div>
+  );
+}
+
+export default App;
