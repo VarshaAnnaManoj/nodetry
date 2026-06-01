27b93c6df42eb68b5a6bde7afc4eb8f632c41139
Manoj Sebastian <varinfoserv@gmail.com>
2026-06-01 02:36:50 -0400

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://backend:3000',
+        target: 'http://backend:5000',
         changeOrigin: true
       }
     }
